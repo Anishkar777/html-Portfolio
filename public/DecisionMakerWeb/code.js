b27b4d34107070890464c843a03edf6174742535
Anishkar777 <anishkar7a46@gmail.com>
2025-12-17 10:44:24 +0530

    Add files via upload

---
+++ public/DecisionMakerWeb/code.js
@@ -0,0 +1,24 @@
+let myList=[];
+function Add(){
+    let input=document.getElementById("choices").value;
+    if(input != ""){
+        myList.push(input);
+        document.getElementById("choices").value="";
+        
+    }
+    display()
+}
+function display(){
+    let options=document.getElementById("options");
+    options.innerHTML = "<h3>Items:</h3>";
+    myList.forEach((item, index) => {
+        options.innerHTML+= index+1 + ":" + item + "<br>";
+
+        
+    });
+}
+function Choice(){
+    let randdd=Math.floor(Math.random() * myList.length);
+    
+    document.getElementById("display").innerHTML="Choose "+myList[randdd];
+}
